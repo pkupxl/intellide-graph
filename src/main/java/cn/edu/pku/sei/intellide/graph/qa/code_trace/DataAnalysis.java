c22639c5acd462c3da5611bcda15d405a63f26eb
pkupxl <bjdxpxl@pku.edu.cn>
2018-12-19 16:18:06 +0800

    统计

--- src/main/java/cn/edu/pku/sei/intellide/graph/qa/code_trace/DataAnalysis.java
+++ src/main/java/cn/edu/pku/sei/intellide/graph/qa/code_trace/DataAnalysis.java
@@ -1,5 +1,4 @@
 package cn.edu.pku.sei.intellide.graph.qa.code_trace;
-
 import cn.edu.pku.sei.intellide.graph.extraction.code_mention.CodeMentionExtractor;
 import cn.edu.pku.sei.intellide.graph.extraction.git.GitExtractor;
 import cn.edu.pku.sei.intellide.graph.extraction.java.JavaExtractor;
@@ -28,7 +27,6 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -38,144 +36,105 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import cn.edu.pku.sei.intellide.graph.extraction.jira.JiraExtractor;
 
 public class DataAnalysis {
-    public static void main(String args[])throws IOException, ParseException {
-        GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File("D:\\Work\\isis"));
 
-        double Classcnt = 0;
-        double count0 = 0;
-        double count1 = 0;
-        double count2 = 0;
-        double count3 = 0;
-        double count4 = 0;
-        double count5 = 0;
-        double count6 = 0;
-        double count7 = 0;
-        double count8 = 0;
-        double count9 = 0;
-        double count10 = 0;
-        double count11 = 0;
-        double count12 = 0;
-        double count13 = 0;
-        double count14 = 0;
-        double count15 = 0;
-        double count16 = 0;
-        double count17 = 0;
-        double count18 = 0;
-        double count19 = 0;
-        double count20 = 0;
-        double count20more = 0;
-double sum=0;
-double sum1=0;
-String projectName="ISIS";
+    public static void Test() throws IOException ,ParseException{
+        GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File("D:\\Work\\hama"));
+
+        final String CONTENT_FIELD = "content";
+        final String ID_FIELD = "id";
+
+
+        int cnt[]=new int[12];
+
+        double Methodcnt=0;
         try (Transaction tx = graphDb.beginTx()) {
             ResourceIterator<Node> iterator = graphDb.getAllNodes().iterator();
             while (iterator.hasNext()) {
                 Node node = iterator.next();
                 if (node.getLabels().iterator().next().name().equals("Class")) {
 
-                    Classcnt++;
-                    Iterator<Relationship> methodIter = node.getRelationships().iterator();
+                    Analyzer analyzer = new StandardAnalyzer();
+                    Directory directory = new RAMDirectory();
+                    IndexWriterConfig config = new IndexWriterConfig(analyzer);
+                    IndexWriter iwriter = new IndexWriter(directory, config);
+                    Document document = new Document();
 
-                    int cnt = 0;
-                    int cnt1=0;
-                    while (methodIter.hasNext()) {
-                        Relationship Metherrelation = methodIter.next();
-                        Node otherNode = Metherrelation.getOtherNode(node);
-                        if (otherNode.hasLabel(GitExtractor.COMMIT) && !Metherrelation.isType(CodeMentionExtractor.CODE_MENTION)) {
+                    Iterator<Relationship> relationIter = node.getRelationships().iterator();
+                    while (relationIter.hasNext()) {
+                        Relationship relation = relationIter.next();
+                        if (relation.isType(CodeMentionExtractor.RELATE_VIA_COMMIT)) {
+                            Node otherNode = relation.getOtherNode(node);
+                            document.add(new StringField(ID_FIELD, otherNode.getProperty("name").toString(), Field.Store.YES));
+                            String content = otherNode.getProperty("summary").toString() + " " + otherNode.getProperty("description").toString();
+                            document.add(new TextField(CONTENT_FIELD, content, Field.Store.YES));
+                            iwriter.addDocument(document);
+                        }
+                    }
+                    iwriter.close();
 
-                            String message=otherNode.getProperty("message").toString();
-                            Pattern pattern = Pattern.compile(projectName+"-(\\d+):(.*)");
-                            Matcher matcher = pattern.matcher(message);
-                            if(matcher.find() && matcher.start()==0 ){
-                               cnt1++;
+                    DirectoryReader ireader = DirectoryReader.open(directory);
+                    IndexSearcher isearcher = new IndexSearcher(ireader);
+                    QueryParser parser = new QueryParser(CONTENT_FIELD, analyzer);
+
+                    Iterator<Relationship> Iter = node.getRelationships().iterator();
+                    while (Iter.hasNext()) {
+                        Relationship relation = Iter.next();
+                        if (relation.isType(JavaExtractor.HAVE_METHOD)) {
+                            Node methodNode = relation.getOtherNode(node);
+                            Methodcnt++;
+
+
+
+                            String name = (String) methodNode.getProperty(JavaExtractor.NAME);
+                            String fullName= (String) methodNode.getProperty(JavaExtractor.FULLNAME);
+                            String q = name.toLowerCase();
+
+
+                            Iterator<Relationship> It = methodNode.getRelationships().iterator();
+                            while(It.hasNext()){
+                                Relationship r =It.next();
+                                if(r.isType(JavaExtractor.METHOD_CALL)){
+                                    Node other=r.getOtherNode(methodNode);
+                                    q=q+" OR "+other.getProperty(JavaExtractor.NAME).toString().toLowerCase();
+                                }
                             }
 
-                            cnt++;
+
+                            Query query = parser.parse(q);
+
+
+                            ScoreDoc[] hits = isearcher.search(query, 10000).scoreDocs;
+                            if (hits.length > 0) {
+                                System.out.println(fullName+":"+hits.length);
+                            }
+
+                            if(hits.length<=10){
+                                cnt[hits.length]++;
+                            }else{
+                                cnt[11]++;
+                            }
+
                         }
                     }
 
-                    sum+=cnt;
-                    sum1+=cnt1;
-
-                    if (cnt1 == 0) {
-                        count0++;
-                    } else if (cnt1 == 1) {
-                        count1++;
-                    } else if (cnt1 == 2) {
-                        count2++;
-                    } else if (cnt1 == 3) {
-                        count3++;
-                    } else if (cnt1 == 4) {
-                        count4++;
-                    } else if (cnt1 == 5) {
-                        count5++;
-                    } else if (cnt1 == 6) {
-                        count6++;
-                    } else if (cnt1 == 7) {
-                        count7++;
-                    } else if (cnt1 == 8) {
-                        count8++;
-                    } else if (cnt1 == 9) {
-                        count9++;
-                    } else if (cnt1 == 10) {
-                        count10++;
-                    } else if (cnt1 == 11) {
-                        count11++;
-                    } else if (cnt1 == 12) {
-                        count12++;
-                    } else if (cnt1 == 13) {
-                        count13++;
-                    } else if (cnt1 == 14) {
-                        count14++;
-                    } else if (cnt1 == 15) {
-                        count15++;
-                    } else if (cnt1 == 16) {
-                        count16++;
-                    } else if (cnt1 == 17) {
-                        count17++;
-                    } else if (cnt1 == 18) {
-                        count18++;
-                    } else if (cnt1 == 19) {
-                        count19++;
-                    } else if (cnt1 == 20) {
-                        count20++;
-                    } else {
-                        count20more++;
-                    }
                 }
-                tx.success();
             }
-
-
-            System.out.println(Classcnt);
-            System.out.println(count0);
-            System.out.println(count1);
-            System.out.println(count2);
-            System.out.println(count3);
-            System.out.println(count4);
-            System.out.println(count5);
-            System.out.println(count6);
-            System.out.println(count7);
-            System.out.println(count8);
-            System.out.println(count9);
-            System.out.println(count10);
-            System.out.println(count11);
-            System.out.println(count12);
-            System.out.println(count13);
-            System.out.println(count14);
-            System.out.println(count15);
-            System.out.println(count16);
-            System.out.println(count17);
-            System.out.println(count18);
-            System.out.println(count19);
-            System.out.println(count20);
-            System.out.println(count20more);
-            System.out.println(sum1/Classcnt);
-
+            tx.success();
         }
+
+        System.out.println(Methodcnt);
+
+        for(int i=0;i<12;++i){
+            System.out.println(i+":"+cnt[i]);
+        }
+    }
+
+
+    public static void main(String args[])throws IOException ,ParseException{
+        Test();
+
     }
 }
